9ad1a840448bd32d2c18acd559434f105f3fd4fc
anilreddy <anilreddy.9291@gmail.com>
2017-08-21 22:40:33 +0530

    added to staging area

---
+++ src/main/java/edu/university/portlet/Testportlet/App.java
@@ -0,0 +1,13 @@
+package edu.university.portlet.Testportlet;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
